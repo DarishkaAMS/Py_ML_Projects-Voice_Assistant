c87b83081b632bfe9634402e0e258fecdf48062e
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-06 07:17:45 +0300

    Select voice and speed

--- darishkaAMS_virtual_assistant.py
+++ darishkaAMS_virtual_assistant.py
@@ -3,10 +3,13 @@ import pyttsx3 as px
 engine = px.init()
 
 voices = engine.getProperty('voices')
-
 # GB_HAZEL_11.0
 engine.setProperty('voice', voices[1].id)
 
-engine.say("Hello, my name is DarishkaAMS. Let's make miracles happen")
+
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 180)
+
+engine.say("Hello, my name is DarishkaAMS. I'm the Intelligent Voice Assistant in Python. Let's make miracles happen")
 engine.runAndWait()
 engine.stop()
